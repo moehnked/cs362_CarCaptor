d642194c3fb1274d9a0273019c24d0f3ee35e29b
Gabor Konczol <konczolg@oregonstate.edu>
2019-04-22 11:04:00 -0700

    Update car class unit test to conform to new design.

--- spec/models/car_spec.rb
+++ spec/models/car_spec.rb
@@ -1,9 +1,10 @@
 require "rails_helper"
 
 RSpec.describe Car do
-  it "a car should be initialized with a coolness value greater than or equal to zero", :skip do
-    car = Car.new
-    expect(car.coolness_value).to be >= 0
+  it "a car should be initialized with a coolness value greater than or equal to zero" do
+    creator = CreateCar.new(year: 1997, make: "Ford", model: "Thing", coolness_value: 9)
+    creator.build
+    expect(creator.car.coolness_value).to be >= 0
   end
   
 end
