a2b394dacf2d7fe5b5502e9bfe29aacb950bd824
moehnked <moehnked@oregonstate.edu>
2019-04-23 23:28:48 -0700

    stuff doesn't work yet

--- app/models/garage.rb
+++ app/models/garage.rb
@@ -1,15 +1,27 @@
+require "car"
+
 class Garage
-	attr_accessor :cars
+	attr_accessor :cars, :total_cars
 
 	def initialize
 		@cars = []
+		@total_cars = 0
 	end
 
 	def total_coolness
 		cars.sum(&:coolness_value)
 	end
 
+	def add(car = Car.new)
+		cars << car
+		total_cars += 1
+	end
+
 	def lame?
 		cars.empty?
+	end
+
+	def car_count
+		total_cars
 	end
 end
--- spec/models/garage_spec.rb
+++ spec/models/garage_spec.rb
@@ -1,15 +1,30 @@
 require "rails_helper"
 
 RSpec.describe Garage do
-	let(:garage) { Garage.new }
-	let(:car) { Car.new }
 
-	it "considers a garage with no cars to be lame", :skip do
-		expect(garage).to be_lame
+	describe "adding cars" do
+		it "keeps a total of the number of cars" do
+			test_garage = Garage.new
+			test_garage.add()
+			expect(test_garage.car_count).to eq(1)
+		end
 	end
 
-	it "considers a garage with cars to be lame if their total coolness is zero", :skip do
-		garage.cars << car
-		expect(garage.total_coolness).to eq(1)
+	describe "happy path" do
+		let(:garage) { Garage.new }
+		let(:car) { Car.new }
+
+		it "considers a garage with no cars to be lame", :skip do
+			expect(garage).to be_lame
+		end
+
+		it "considers a garage with cars to be lame if their total coolness is zero", :skip do
+			garage.cars << car
+			expect(garage.total_coolness).to eq(1)
+		end
+	end
+
+	describe "unhappy path" do
+
 	end
 end
