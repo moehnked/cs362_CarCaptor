70f3abb0cb40a5416534e989789fb39a0e47c0b5
Gabor Konczol <konczolg@oregonstate.edu>
2019-05-05 22:19:24 -0700

    Fix previously skipped test.

--- spec/system/add_car_spec.rb
+++ spec/system/add_car_spec.rb
@@ -1,12 +1,15 @@
 require "rails_helper"
 
 RSpec.describe "adding a car", type: :system do
-  skip it "allows a user to create a car" do
+  it "allows a user to create a car" do
     visit new_car_path
     fill_in "Year", with: "1999"
     fill_in "Make", with: "Ford"
     fill_in "Model", with: "Mustang"
     fill_in "Coolness value", with: 5
+    fill_in "Horsepower", with: 260
+    fill_in "Torque", with: 302
+    fill_in "Weight", with: 3273
     click_on("Create Car")
     visit cars_path
     @car = Car.find_by(model: "Mustang")
@@ -14,5 +17,8 @@ RSpec.describe "adding a car", type: :system do
     expect(page).to have_selector("#car_#{@car.id} .make", text: "Ford")
     expect(page).to have_selector("#car_#{@car.id} .model", text: "Mustang")
     expect(page).to have_selector("#car_#{@car.id} .coolness_value", text: "5")
+    expect(page).to have_selector("#car_#{@car.id} .horsepower", text: "260")
+    expect(page).to have_selector("#car_#{@car.id} .torque", text: "302")
+    expect(page).to have_selector("#car_#{@car.id} .weight", text: "3273")
   end
 end
