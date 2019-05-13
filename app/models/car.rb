a9d10a6d55382bc8e2832e29303441e65cda77af
moehnked <moehnked@oregonstate.edu>
2019-05-13 11:47:05 -0700

    added info method to car class

--- app/models/car.rb
+++ app/models/car.rb
@@ -15,6 +15,10 @@ class Car < ApplicationRecord
       self.horsepower.fdiv(self.weight)
     end
 
+    def info
+      self.year.to_s + " " + self.make + " " + self.model + ", hp " + self.horsepower.to_s + ", tq " + self.torque.to_s + ", weight " + self.weight.to_s
+    end
+
     def does_opponent_win_simple_drag_race?(opponent_car)
     if opponent_car.calculate_power_to_weight_ratio > self.calculate_power_to_weight_ratio
       true
